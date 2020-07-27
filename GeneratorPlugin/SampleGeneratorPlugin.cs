984bd6c33b4d31dc3782e3d54bdf3734ec952a6b
Yauheni Rubakhau <yauheni_rubakhau@epam.com>
2020-07-27 15:54:27 +0300

    Upload test proj

---
+++ GeneratorPlugin/SampleGeneratorPlugin.cs
@@ -0,0 +1,20 @@
+﻿using GeneratorPlugin;
+using Gherkin;
+using TechTalk.SpecFlow.Generator.Plugins;
+using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.Parser;
+using TechTalk.SpecFlow.UnitTestProvider;
+
+[assembly:GeneratorPlugin(typeof(SampleGeneratorPlugin))]
+
+namespace GeneratorPlugin
+{
+    public class SampleGeneratorPlugin : IGeneratorPlugin
+    {
+        public void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters,
+            UnitTestProviderConfiguration unitTestProviderConfiguration)
+        {
+            var pars = new SpecFlowGherkinParser(new GherkinDialectProvider());
+        }
+    }
+}
